9d4a8d05cb969c8687b7172370276cd7520493a9
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-19 18:02:16 +0000

    Added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢️ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴️ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual(1, 1);
+assertEqual(1, 5);
+assertEqual(1 + 2, 3);
+assertEqual(1, '1');
+assertEqual('Jillian', "Jillian");
+assertEqual('Jill' + 'ian', 'Jillian');
+assertEqual('Jill', 'Jillian');
