d577e1cc208418cb11b303fa545306aef39a6445
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-20 20:29:58 +0000

    Added flatten function to flatten nested arrays (1 nesting level only)

---
+++ flatten.js
@@ -0,0 +1,50 @@
+//FUNCTION IMPLMENTATION
+const flatten = function(source) {
+  let result = [];
+
+  for (const item of source) {
+    if (Array.isArray(item)) {
+      for (const innerItem of item) {
+        result.push(innerItem);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+// eqArrays and assertArrasEqual, for testing
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TEST CODE
+assertArraysEqual(flatten([1, [2, 3]]), [1, 2, 3]);
+assertArraysEqual(flatten([1, 2, 3]), [1, 2, 3]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(['a', ['b', 'c']]), ['a', 'b', 'c']);
+
+//Test whether input array is modified
+const testArr = [1, [2, 3]];
+flatten(testArr);
+assertArraysEqual(testArr[1], [2, 3]) //assertArraysEqual cannot handle nested arrays, therefor just checking individual array element
