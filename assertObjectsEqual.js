1dce575191cc5597ee0fd6dec6c76283ed9cb989
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-21 02:01:29 +0000

    Completed assertObjectsEqual by using inspect to improve output

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,9 +1,10 @@
 //FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
   if (eqObjects(actual, expected)) {
-    console.log(`🟢️ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢️ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
