ef3a3c69d2af64a0738cd03c52d9c67747d0a8dd
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-21 01:30:32 +0000

    Initial set-up of eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,22 @@
+//assertEqual function imported for testing
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION IMPLEMENTATION
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
+//TEST CODE
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
