23213494877393ea25da846c486e3a0edc88c914
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-21 00:36:15 +0000

    Added countOnly function to count only certain items from an array

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+//assertEqual function imported for testing
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION IMPLEMENTATION
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if(!results[item]) {results[item] = 0;}
+     results[item] += 1;
+    }
+  };
+
+  return results;
+};
+
+//TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
