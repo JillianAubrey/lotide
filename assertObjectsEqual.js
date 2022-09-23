efb24263d9a6c57c886b3c743052f6b10a928456
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-23 21:39:00 +0000

    Removed else from asserObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -3,9 +3,9 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`🟢️ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`🔴️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return;
   }
+  console.log(`🔴️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
 // eqObjects, a helper function
