634de71184d9ed1162a637d155d100c783cdecec
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 20:18:32 +0000

    Implemented mocha and chai testing for eqType

--- test/eqTypeTest.js
+++ test/eqTypeTest.js
@@ -1,12 +1,18 @@
+const assert = require('chai').assert;
 const eqType = require('../eqType');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqType(5, 6), 'number');
-assertEqual(eqType('abc', 'def'), 'string');
-assertEqual(eqType([1,2], []), 'array');
-assertEqual(eqType({}, {key: 0}), 'object');
-assertEqual(eqType([], {key: 0}), false);
-assertEqual(eqType(5, '5'), false);
-assertEqual(eqType(5, {5: 5}), false);
-assertEqual(eqType(5, [5]), false);
-assertEqual(eqType(5, undefined), false);
+describe("#eqType", () => {
+  it("returns the type when item1 and item2 are of the same type", () => {
+    assert.strictEqual(eqType(5, 6), 'number');
+    assert.strictEqual(eqType('abc', 'def'), 'string');
+    assert.strictEqual(eqType([1,2], []), 'array');
+    assert.strictEqual(eqType({}, {key: 0}), 'object');
+  });
+  it("returns false when item1 and item2 have different types", () => {
+    assert.strictEqual(eqType([], {key: 0}), false);
+    assert.strictEqual(eqType(5, '5'), false);
+    assert.strictEqual(eqType(5, {5: 5}), false);
+    assert.strictEqual(eqType(5, [5]), false);
+    assert.strictEqual(eqType(5, undefined), false);
+  });
+});
