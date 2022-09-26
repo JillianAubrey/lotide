5675713994af22983770f89a5944616cc3ecb7dc
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 17:19:59 +0000

    Updated middleTest to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#tail", () => {
+  it("returns [] for an array with 2 or fewer items", () => {
+    assert.deepEqual(middle([]), []);
+    assert.deepEqual(middle([1]), []);
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns the middle item for an array with an odd number of items", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns both middle items for an array with an even number of items", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("does not modify the original array", () => {
+    const arr = [1,2,3];
+    middle(arr);
+    assert.deepEqual(arr, [1,2,3]);
+  });
+});
