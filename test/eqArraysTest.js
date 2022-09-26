7bab72faf4aa320ed0c3cf794659e6bdd5c54878
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 20:15:59 +0000

    Implemented moch and chai testing for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,15 +1,18 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1], [1]), true);
-assertEqual(eqArrays([1], [1, 2]), false);
-
-assertEqual(eqArrays([[1]], [[1]]), true);
-assertEqual(eqArrays([[1]], [1]), false);
+describe("#eqArrays", () => {
+  it("returns true when arr1 and arr2 are the same", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+    assert.strictEqual(eqArrays([], []), true);
+    assert.strictEqual(eqArrays([1], [1]), true);
+    assert.strictEqual(eqArrays([[1]], [[1]]), true);
+  });
+  it("returns false when arr1 and arr2 are differnt", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  assert.strictEqual(eqArrays([1], [1, 2]), false);
+  assert.strictEqual(eqArrays([[1]], [1]), false);
+  });
+});
