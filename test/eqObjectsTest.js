4f50b5d79cd3e46a7fb1b840e15bfb4e83a742fe
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 20:26:10 +0000

    Implemented mocha and chai testing for eqObjects

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,19 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
 const ac = { a: "1", c: "3"};
-assertEqual(eqObjects(ab, ac), false);
-
 const acNum = { a: 1, c: 3};
-assertEqual(eqObjects(acNum, ac), false);
+const azb = { a: { z: 1 }, b: 2 };
+const baz = { b: 2, a: { z: 1 }};
+const ayzb = { a: { y: 0, z: 1 }, b: 2 }
 
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),true);
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false);
+
+describe("#eqObjects", () => {
+  it("returns true when obj1 and obj2 are the same", () => {
+    assert.strictEqual(eqObjects(ab, ba), true);
+    assert.strictEqual(eqObjects(azb, baz), true);
+    assert.strictEqual(eqObjects({}, {}), true);
+  });
+  it("returns false when obj1 and obj2 are differnt", () => {
+    assert.strictEqual(eqObjects(ab, abc), false);
+  assert.strictEqual(eqObjects(ab, ac), false);
+  assert.strictEqual(eqObjects(ac, acNum), false);
+  assert.strictEqual(eqObjects(azb, ayzb), false);
+  });
+});
