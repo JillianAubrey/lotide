b974fbfef998f7824350e13c87ca3cdac0c922a4
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 20:12:27 +0000

    Implemented mocha and chai testing for deepEqual

--- test/deepEqualTest.js
+++ test/deepEqualTest.js
@@ -1,17 +1,16 @@
+const assert = require('chai').assert;
 const deepEqual = require('../deepEqual');
-const assertEqual = require('../assertEqual');
 
-assertEqual(deepEqual([{}], [{}]), true);
-assertEqual(deepEqual([{a: 1}], [{a: 1}]), true);
-assertEqual(deepEqual([{a: 1}, {b:2}], [{b:2},{a: 1}]), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(deepEqual(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(deepEqual(cd, cd2), false);
-
-const cde = { c: "1", d: ["2", 3], e: {key: 'value'} };
-const ced = { c: "1", e: {key: 'value'}, d: ["2", 3] };
-assertEqual(deepEqual(cde, ced), true);
+describe("#deepEqual", () => {
+  it("returns true when item1 and item2 are the same", () => {
+    assert.strictEqual(deepEqual([{}], [{}]), true);
+    assert.strictEqual(deepEqual([{a: 1}], [{a: 1}]), true);
+    assert.strictEqual(deepEqual({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" }), true);
+    assert.strictEqual(deepEqual({ c: "1", d: ["2", 3], e: {key: 'value'} }, { c: "1", e: {key: 'value'}, d: ["2", 3] }), true);
+    assert.strictEqual(deepEqual({a: [{b: [{c: [{d: [1,2,3]}]}]}]}, {a: [{b: [{c: [{d: [1,2,3]}]}]}]}), true);
+  });
+  it("returns false when item1 and item2 are differnt", () => {
+    assert.strictEqual(deepEqual({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4] }), false);
+    assert.strictEqual(deepEqual({a: [{b: [{c: [{d: [1,2,3]}]}]}]}, {a: [{b: [{c: [{d: [1,2,3,4]}]}]}]}), false);
+  });
+});
