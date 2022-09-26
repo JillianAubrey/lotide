37ec07a6ab1e1fb9bf89fe5c7aa6d4d97cad66a2
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 16:24:17 +0000

    Added test cases for eqType

---
+++ test/eqTypeTest.js
@@ -0,0 +1,12 @@
+const eqType = require('../eqType');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqType(5, 6), 'number');
+assertEqual(eqType('abc', 'def'), 'string');
+assertEqual(eqType([1,2], []), 'array');
+assertEqual(eqType({}, {key: 0}), 'object');
+assertEqual(eqType([], {key: 0}), false);
+assertEqual(eqType(5, '5'), false);
+assertEqual(eqType(5, {}), false);
+assertEqual(eqType(5, []), false);
+assertEqual(eqType(5, undefined), false);
