ad5b43839b589b4c295e1da80f0cdfff6678f204
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-26 16:25:19 +0000

    Updated test cases for eqType

--- test/eqTypeTest.js
+++ test/eqTypeTest.js
@@ -7,6 +7,6 @@ assertEqual(eqType([1,2], []), 'array');
 assertEqual(eqType({}, {key: 0}), 'object');
 assertEqual(eqType([], {key: 0}), false);
 assertEqual(eqType(5, '5'), false);
-assertEqual(eqType(5, {}), false);
-assertEqual(eqType(5, []), false);
+assertEqual(eqType(5, {5: 5}), false);
+assertEqual(eqType(5, [5]), false);
 assertEqual(eqType(5, undefined), false);
