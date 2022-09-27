a9dc88af149c399fcf59a8d2287cc8c31c257fad
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 04:50:46 +0000

    Implemented mocha and chai testing for letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -16,11 +16,3 @@ const letterPositions = function(str) {
 };
 
 module.exports = letterPositions;
-
-// //TEST CODE
-// let testStr = 'LHL';
-// let result = letterPositions(testStr);
-// assertArraysEqual(result['l'], [0, 2]);
-// assertArraysEqual(result['h'], [1]);
-// //Test that string is not affected
-// assertArraysEqual([testStr], ['LHL']);
---
+++ test/letterPositionsTest.js
@@ -0,0 +1,19 @@
+const assert = require('chai').assert;
+const letterPositions = require('../letterPositions');
+
+describe("#flatten", () => {
+  it("returns an object with positions of each letter", () => {
+    let testStr = 'LHL';
+    let actual = letterPositions(testStr);
+    let expected = {
+      l: [0, 2],
+      h: [1],
+    };
+    assert.deepEqual(actual, expected);
+  });
+  it("does not modify the original array", () => {
+    let testStr = 'LHL';
+    letterPositions(testStr);
+    assert.strictEqual(testStr, 'LHL');
+  });
+});
