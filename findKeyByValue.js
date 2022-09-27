5537fd68a26d595bd6c441c58b9cce490c81ffac
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-27 04:52:01 +0000

    Removed commented test code from findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,13 +7,3 @@ const findKeyByValue = function(obj, val) {
 };
 
 module.exports = findKeyByValue;
-
-// //TEST CODE
-// const bestTVShowsByGenre = {
-//   sci_fi: "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama:  "The Wire"
-// };
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
